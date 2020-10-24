dd87fc0151a2e047ad0a7ca774d312f5432e3cf8
Marcin Kloczkowski <marcin@kloczkowski.net>
2020-10-24 17:27:06 +0200

    Initial commit

---
+++ skm-api/src/test/java/pl/edu/pjwstk/skmapi/SkmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.skmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
