c78a622059f1f93a71bb82a86381e1634f0be656
Marcin Kloczkowski <marcin@kloczkowski.net>
2020-10-27 15:18:18 +0100

    + Added Java Faker library * Person generates random name

--- skm-api/src/main/java/pl/edu/pjwstk/skmapi/model/Person.java
+++ skm-api/src/main/java/pl/edu/pjwstk/skmapi/model/Person.java
@@ -1,13 +1,16 @@
 package pl.edu.pjwstk.skmapi.model;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
 public class Person {
 
     private final String name;
     private final TrainStation destinationStation;
 
     public Person(TrainStation startingStation, Direction direction) {
-        name = "Will Smith";
-
+        name = generateRandomName();
         int randomStationNumber;
         if (direction.getDirection() == Direction.START_TO_END) {
             randomStationNumber = Randomizer.getRandomNumberInRange(startingStation.getId() + 1, TrainStation.values().length - 1);
@@ -23,5 +26,12 @@ public class Person {
 
     public TrainStation getDestinationStation() {
         return destinationStation;
+    }
+
+    private String generateRandomName() {
+        Faker faker = new Faker(new Locale("pl"));
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        return firstName + " " + lastName;
     }
 }
