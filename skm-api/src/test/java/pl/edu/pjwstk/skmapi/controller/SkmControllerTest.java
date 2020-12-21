7d3fb89fd7f4e52f209d477d6caca3cdb7a36856
bdstr <marcin@kloczkowski.net>
2020-12-21 21:54:43 +0100

    + Added SkmControllerTest

---
+++ skm-api/src/test/java/pl/edu/pjwstk/skmapi/controller/SkmControllerTest.java
@@ -0,0 +1,33 @@
+package pl.edu.pjwstk.skmapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.edu.pjwstk.skmapi.service.SkmService;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SkmController.class)
+class SkmControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    SkmService skmService;
+
+    @Test
+    void moveForwardCallsMethodInSkmService() throws Exception {
+        String uri = "/move";
+
+        mockMvc.perform(post(uri))
+                .andExpect(status().isOk());
+
+        verify(skmService).moveSimulationStepForward();
+    }
+}
