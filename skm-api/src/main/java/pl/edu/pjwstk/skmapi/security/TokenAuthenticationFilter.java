e60cf6bd8c1d304720487b26e38e35f44fa77a24
Marcin Kloczkowski <marcin@kloczkowski.net>
2021-01-23 18:21:14 +0100

    + Added Spring Security with JWT implementation (currently hard-coded user)

---
+++ skm-api/src/main/java/pl/edu/pjwstk/skmapi/security/TokenAuthenticationFilter.java
@@ -0,0 +1,72 @@
+package pl.edu.pjwstk.skmapi.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static pl.edu.pjwstk.skmapi.security.util.SecurityConstants.EXPIRATION_TIME;
+import static pl.edu.pjwstk.skmapi.security.util.SecurityConstants.SECRET;
+
+public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    AuthenticationManager authenticationManager;
+
+    public TokenAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+
+        setFilterProcessesUrl("/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user.getUsername(),
+                    user.getPassword(),
+                    Collections.emptyList());
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException {
+        var subject = auth.getPrincipal().toString();
+
+        List<String> authorities = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        var token = JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .withClaim("authorities", authorities)
+                .sign(Algorithm.HMAC512(SECRET));
+
+        var payload = new LinkedHashMap<String, Object>();
+        payload.put("username", subject);
+        payload.put("token", token);
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(payload));
+        response.getWriter().flush();
+    }
+}
